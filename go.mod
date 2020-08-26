f7382a1a926b88fbc565077f8a9dc7ac0d0e3e5d
Igor Mandrigin <i@mandrigin.ru>
2020-08-26 15:51:01 +0200

    draft on how code could look like

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mandrigin/turbo-geth-binary-tree-hashes
 
 go 1.15
+
+require github.com/ledgerwatch/turbo-geth v0.0.0-20200826120651-714e5041e28d
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ledgerwatch/turbo-geth/eth/stagedsync/stages"
+)
 
 func main() {
+	s := stagedsync.DefaultStages
+	s = replaceStage(stages.IntermediateHashes, binaryIntermediateHashes)
+	core := turbo.NewCore(
+		stagedsync.New(s)
+	)
+	core.Run()
 	fmt.Println("hallo")
+}
+
+func replaceStage(source []stagedsync.Stage, id stages.Stage, stage stagedsync.Stages) []stagedsync.Stage {
+	panic("implement me")
 }
